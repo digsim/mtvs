0202304669d6fc64c7dde032f0cc62c95cbb693f
Andreas Ruppen <andreas.ruppen@adnovum.ch>
2017-02-24 20:11:38 +0100

    Raised version number to 1.2.5.dev0 for next release

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ def read(fname):
 
 setup(
     name="mtvs",
-    version="1.2.4",
+    version="1.2.5.dev0",
     author="Andreas Ruppen",
     author_email="andreas.ruppen@gmail.com",
     description="Manages Kodi TVShows",
